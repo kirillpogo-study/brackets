9d21463a7e90d8794650e138ef8532746d406133
Kirill Pogorelov <23706384+kirillpogo-study@users.noreply.github.com>
2020-10-14 18:27:42 +0300

    feat: balanced brackets function

--- src/index.js
+++ src/index.js
@@ -1,3 +1,24 @@
-module.exports = function check(str, bracketsConfig) {
-  // your solution
+module.exports = function check (str, bracketsConfig) {
+  let prevStr = str
+  bracketsConfig = bracketsConfig.map(brackets => brackets[0] + brackets[1])
+  let counter = 0
+
+  do {
+    bracketsConfig.forEach(pair => {
+      const index = str.indexOf(pair)
+      if ((str.indexOf(pair)) > -1) {
+        counter = 0
+        prevStr = str
+        str = str.split('')
+        str.splice(index, 2)
+        str = str.join('')
+      } else {
+        if (prevStr === str) counter++
+        prevStr = str
+      }
+    })
+  } while ((counter < bracketsConfig.length) && (str.length !== 0))
+
+  if (str.length !== 0) return false
+  return true
 }
